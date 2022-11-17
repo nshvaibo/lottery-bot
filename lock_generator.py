fd3e0129f0b19c2dcf95aea103e1b255df4bcb3e
Nikita <n.shvaybovich@gmail.com>
2022-11-16 23:54:47 -0500

    Renamed groups lock generator

---
+++ lock_generator.py
@@ -0,0 +1,38 @@
+from threading import Lock
+from weakref import WeakValueDictionary
+
+class MutexRAII:
+    def __init__(self) -> None:
+        self.m = Lock()
+
+    def __del__(self):
+        """Release lock upon destruction of the object"""
+        if self.m.locked():
+            self.m.release()
+
+class LockGenerator:
+    """Provides an RAII mutex for any ID based group of entities"""
+
+    def __init__(self) -> None:
+        # Main lock for protecting locks for the individual locks
+        self.general_lock = Lock()
+        
+        # Map of IDs and their corresponding mutexes if they exist
+        self.locks = WeakValueDictionary()
+    
+    def get_lock(self, id):
+        """Returns a lock for this entity in the group when shared state can be accessed"""
+        self.general_lock.acquire()
+
+        # Check if there is a lock for this user
+        if id in self.locks:
+            lock = self.locks[id]
+        else: # Otherwise create a new lock
+            lock = Lock()
+            self.locks[id] = lock
+
+        lock.acquire()
+
+        self.general_lock.release()
+
+        return lock
